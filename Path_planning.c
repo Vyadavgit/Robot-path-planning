23cdb2ef3113550fbfc4b9635ebe0c7caeb56d23
Vyadavgit <>
2021-10-28 22:35:59 -0500

    debug needed to fix pathPlanner and avoidobstacles

--- Path_planning.c
+++ Path_planning.c
@@ -13,18 +13,22 @@
 int num_obstacles = 13; /* number of obstacles */
 
 double obstacle[MAX_OBSTACLES][2] = /* obstacle locations */
-{{0.61, 2.743},{0.915, 2.743},{1.219, 2.743},{1.829, 1.219},
-{1.829, 1.524},{ 1.829, 1.829}, {1.829, 2.134},{2.743, 0.305},
-{2.743, 0.61},{2.743, 0.915},{2.743, 2.743},{3.048, 2.743},
-{3.353, 2.743},
-{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},
-{-1,-1},{-1,-1},{-1,-1}};
+{{0.305*2, 0.305*2},{0.305*3, 0.305*3},{0.305*4, 0.305*4},{-1,-1},{-1,-1},
+{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},
+{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},
+{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},
+{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
 
-double start[2] = {0.305, 1.219}; /* start location */
-double goal[2] = {3.658, 1.829}; /* goal location */
+double start[2] = {0.305*1, 0.305*1}; /* start location */
+double goal[2] = {0.305*5,0.305*4}; /* goal location */
 
 int currAngle = 270;
 double currLocation[2]={0.305, 1.219}; //TODO equalize to start
+int alreadyFoundObstacle[MAX_OBSTACLES]; // only stores the rows of obstacle array (rows of obstacles that are found to 
+                                        // be nearest to start positions during path planning)
+int i=0;
+
+double sqrt(double arg);
 
 void moveStraight(double distance){
 	// Notes: 360 motor rotations = 1 wheel rotation
@@ -171,6 +175,100 @@ void moveX1Y1toX2Y2(double initialLocation[2],double goalLocation[2]){
 	moveAlongYaxis(initialLocation, goalLocation);
 }
 
+float calculateDistance(double x1y1[2], double x2y2[2]){
+	double d;
+	d = sqrt((x2y2[0]-x1y1[0])*(x2y2[0]-x1y1[0])+(x2y2[1]-x1y1[1])*(x2y2[1]-x1y1[1]));
+    return d;
+}
+
+int obstacleFound(int r){
+    int b;
+    int m;
+    for(m=0; m<i; m++){
+        if (r == alreadyFoundObstacle[m]){
+            b=1;
+        }
+        else {
+            b=0;
+        }
+    }
+    return b;
+}
+
+void pathPlanner_findNearestObstacle(double startPosition[2], double obstaclesPositions[MAX_OBSTACLES][2]){ //finds nearest obstacles in the order: start-->obstacle-->...-->obstacle-->goal
+                              // i.e finds obstacle nearest to goal, then next obstacle nearest to current obstacle...and
+                              // lastly finds goal location if it is the nearest one
+                              // it skips the obstacles already found while looking for another nearest one
+    double temp[2];
+    double nearestLocation[2];
+    int closestCounter;
+    double min;
+    double lastmin = 100; //arbitrary distance that is greater than any distance between any two coordinates
+                          // in the given frame (or greater than the diagonal of the given frame as it is the longest distance in the frame)
+    double distanceToGoal;
+
+    int row;
+    for (row=0; row<num_obstacles; row++){  // iterate through all obstacles to find the nearest obstacle
+                                                // to the start position(the nearest obstacle found is the start position for next iterations through remaining obstacles)
+
+        if (obstacleFound(row)==0){ // look for the closest obstacles only from the set of obstacles that haven't been found as closest before
+            temp[0] = obstaclesPositions[row][0];
+            temp[1] = obstaclesPositions[row][1];
+
+            min = calculateDistance(startPosition,temp);
+            if (min<=lastmin){
+                lastmin = min;
+                closestCounter = row;
+            }
+        }
+    }
+    nearestLocation[0] = obstaclesPositions[closestCounter][0];
+    nearestLocation[1] = obstaclesPositions[closestCounter][1];
+
+    // after the nearest obstacle is found, check if the goal is nearer than the nearest obstacle found
+    distanceToGoal =  calculateDistance(startPosition, goal);
+    if (distanceToGoal<=lastmin){
+        nearestLocation[0] = goal[0];
+        nearestLocation[1] = goal[1];
+    }
+
+    if((nearestLocation[0] != goal[0]) && (nearestLocation[1] != goal[1])){
+        alreadyFoundObstacle[i]=closestCounter; // storing rows of the obstacles already found i.e plan the path
+        i++; // total obstacles found = i, last obstacle position = i-1
+    }
+
+    TermPrintf("Nearest obstacle: (%f, %f)\n",nearestLocation[0],nearestLocation[1]);
+}
+
+void avoidObstacles(double startPosition[2], double obstaclesPositions[MAX_OBSTACLES][2],double goalPosition[2]){
+	// 1. find the nearest obstacle from start
+    // 2. move to that obstacle(with safety margin 0.61)
+    // 1.. repeat i.e. find the next obstacle closest to the new start position
+    // 2.. repeat i.e. move to that obstacle(with safety margin 0.61)
+    // 1,2.. repeat until you reach the last obstacle that is closest to the new start position (means there are no other obstacles that is
+    // closer to the current obstacle than the goal itself)
+    // 3. lastly move to the goal
+
+    // 1.
+    double tempArr[2];
+    int j;
+    for(j=0; j<i; j++){
+        tempArr[0] =  obstaclesPositions[alreadyFoundObstacle[j]][0];
+        tempArr[1] =  obstaclesPositions[alreadyFoundObstacle[j]][1];
+        moveX1Y1toX2Y2(startPosition,tempArr);
+        startPosition[0]=tempArr[0];
+        startPosition[1]=tempArr[1];
+    }
+
+    moveX1Y1toX2Y2(startPosition, goal);
+}
+
+// void destroyArray(float** arr)
+// {
+//     free(*arr);
+//     free(arr);
+// }
+
 
 int main(void)
 {
@@ -189,10 +287,12 @@ int main(void)
 
 
 	// Visiblity graph algorithm
-	double X1Y1[2] = {0,0};
-	double X2Y2[2] = {0.305*3,0.305*3};
+	// double X1Y1[2] = {0,0};
+	// double X2Y2[2] = {0.305*3,0.305*3};
 
-    moveX1Y1toX2Y2(X1Y1, X2Y2);
+    // moveX1Y1toX2Y2(X1Y1, X2Y2);
+    pathPlanner_findNearestObstacle(start, obstacle);
+    avoidObstacles(start,obstacle,goal);
 
 	Wait(500);
 	FreeEV3();
